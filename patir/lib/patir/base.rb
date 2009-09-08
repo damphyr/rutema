b2bf10e1a3b8e740c574df7f1460ea7ab725fe40
Vassilis Rizopoulos <riva@vassilis-rizopouloss-macbook-pro-15.local>
2009-09-08 09:04:20 +0200

    0.6.5

--- patir/lib/patir/base.rb
+++ patir/lib/patir/base.rb
@@ -6,7 +6,7 @@ module Patir
   module Version
     MAJOR=0
     MINOR=6
-    TINY=4
+    TINY=5
     STRING=[ MAJOR, MINOR, TINY ].join( "." )  	
   end
   #Error thrown usually in initialize methods when missing required parameters
