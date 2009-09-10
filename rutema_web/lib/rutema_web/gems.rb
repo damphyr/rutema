54f27bdbf3e7f4a2a77a628f6fb5612e56a1c545
Vassilis Rizopoulos <riva@vassilis-rizopouloss-macbook-pro-15.local>
2009-09-10 17:19:32 +0200

    updated gem dependencies, upgraded gruff

--- rutema_web/lib/rutema_web/gems.rb
+++ rutema_web/lib/rutema_web/gems.rb
@@ -1,7 +1,4 @@
-gem 'sinatra','=0.9.2'
-gem 'activerecord','=2.3.2'
-gem 'rutema',"=1.0.5"
-gem 'patir',"=0.6.1"
-gem 'ruport','=1.6.1'
-gem 'acts_as_reportable','=1.1.1'
-gem 'gruff',"=0.3.4"
+gem 'sinatra','0.9.2'
+gem 'rutema',"1.0.6"
+gem 'ruport','1.6.1'
+gem 'gruff',"0.3.6"
--- rutema_web/lib/rutema_web/main.rb
+++ rutema_web/lib/rutema_web/main.rb
@@ -8,7 +8,7 @@ module RutemaWeb
   module Version
     MAJOR=1
     MINOR=0
-    TINY=1
+    TINY=2
     STRING=[ MAJOR, MINOR, TINY ].join( "." )
   end
   #Starts App
