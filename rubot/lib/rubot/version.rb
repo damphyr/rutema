b734761f78c7dfd51cb23330f1a21f4b4e5431cd
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2012-11-28 15:19:28 +0200

    removed everything that belongs to a separate repository

--- rubot/lib/rubot/version.rb
+++
@@ -1,8 +0,0 @@
-module Rubot
-  module Version
-    MAJOR=0
-    MINOR=1
-    TINY=0
-    STRING=[ MAJOR, MINOR, TINY ].join( "." )  	
-  end
-end
--- rubot/overseer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rf test/overseer/example/overseer.db
-rm -rf test/overseer/example/rubot_overseer.log
-ruby -Ilib/ bin/rubot overseer -b test/overseer/example  --no-log
