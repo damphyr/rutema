dd555cccab7dc60e72bef747353fa994b5bc7bff
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2015-03-06 12:59:49 +0100

    dead comments

--- lib/rutema/core/runner.rb
+++ lib/rutema/core/runner.rb
@@ -80,21 +80,4 @@ module Rutema
       end
     end
   end
-  
-  #StepRunner halts before every step and asks if it should be executed or not.
-  # class StepRunner<Runner
-  #   def initialize setup=nil, teardown=nil,logger=nil
-  #     @questioner=HighLine.new
-  #     super(setup,teardown,logger)
-  #   end
-  #   def run_step step
-  #     if @questioner.agree("Execute #{step.to_s}?")
-  #       return super(step)
-  #     else
-  #       msg="#{step.number} - #{step.step_type} - #{step.status}"
-  #       @logger.info(msg)
-  #       return step
-  #     end
-  #   end
-  # end
 end
