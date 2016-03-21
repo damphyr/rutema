5e6a2f716c99b4ca8e28e170e2438f892fe967bb
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2016-03-21 12:38:08 +0100

    Test the summary reporter

--- test/test_reporters.rb
+++ test/test_reporters.rb
@@ -21,5 +21,28 @@ module TestRutema
 
       assert_equal("<?xml version='1.0'?><testsuite errors='0' failures='0' tests='0' time='0.000157'><testcase name='_setup_' time='0.000157'/></testsuite>", junit_reporter.process_data([],states,[]))
     end
+
+    def test_summary
+      #Rutema::Utilities.expects(:write_file).returns("OK")
+      configuration = mock()
+      configuration.expects(:reporters).returns(configuration)
+      configuration.expects(:fetch).returns({:class=>Rutema::Reporters::JUnit,"filename"=>"rutema.junit.xml"})
+      configuration.stubs(:context).returns({})
+      dispatcher=mock()  
+
+      reporter=Rutema::Reporters::Summary.new(configuration,dispatcher)
+      assert_equal(0,reporter.report([],{},[]))
+      states={"_setup_"=>Rutema::ReportState.new(
+          Rutema::RunnerMessage.new({"timestamp"=>Time.now, "duration"=>0.000157, "status"=>:success, "steps"=>[]})
+        )
+      }
+      assert_equal(0,reporter.report([],states,[]))
+
+      states={"_setup_"=>Rutema::ReportState.new(
+          Rutema::RunnerMessage.new({"timestamp"=>Time.now, "duration"=>0.000157, "status"=>:error, "steps"=>[]})
+        )
+      }
+      assert_equal(1,reporter.report([],states,[]))
+    end
   end
 end
