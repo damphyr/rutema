2365191269f598f00cf40a755f83301ce5c668da
markuspg <markuspg@users.noreply.github.com>
2020-08-13 00:34:01 +0200

    Further tighten configuration test

---
+++ examples/specs/suite_teardown.spec
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<specification name="suite_teardown">
+<title>Suite teardown specification</title>
+<description>Runs after a suite</description>
+<scenario>
+	<echo text="Tearing down suite"/>
+</scenario>
+</specification>
--- test/test_configuration.rb
+++ test/test_configuration.rb
@@ -10,14 +10,20 @@ require 'mocha/test_unit'
 FULL_CONFIG =<<-EOT
   configure do |cfg|
     cfg.check = 'check.spec'
-    cfg.context = { key: 'value' }
+    cfg.context = { key_a: 'A value' }
+    cfg.context = { key_b: 'Another value' }
+    cfg.context = { key_a: 'Oops', key_c: 'One more value' }
     cfg.parser = { class: Rutema::Parsers::SpecificationParser }
     cfg.path = { name: 'doc', path: '/usr/share/doc' }
     cfg.path = { name: 'src', path: '/usr/src' }
     cfg.reporter = { class: Rutema::Reporters::BlockReporter }
-    cfg.tests = ['T001.spec']
+    cfg.reporter = { class: Rutema::Reporters::EventReporter }
+    cfg.runner = { class: Rutema::Runners::Default }
     cfg.setup = 'setup.spec'
+    cfg.suite_setup = 'suite_setup.spec'
+    cfg.suite_teardown = 'suite_teardown.spec'
     cfg.teardown = 'teardown.spec'
+    cfg.tests = ['T001.spec']
     cfg.tool = { name: 'cat', path: '/usr/bin/cat', configuration: {} }
     cfg.tool = { name: 'echo', path: '/usr/bin/echo', configuration: { param: '-n' } }
   end
@@ -40,19 +46,36 @@ module TestRutema
       cfg = 'foo.cfg'
       File.expects(:read).with('full.rutema').returns(FULL_CONFIG)
       File.expects(:exist?).with(File.expand_path('check.spec')).returns(true)
-      File.expects(:exist?).with(File.expand_path('teardown.spec')).returns(true)
       File.expects(:exist?).with(File.expand_path('setup.spec')).returns(true)
+      File.expects(:exist?).with(File.expand_path('suite_setup.spec')).returns(true)
+      File.expects(:exist?).with(File.expand_path('suite_teardown.spec')).returns(true)
+      File.expects(:exist?).with(File.expand_path('teardown.spec')).returns(true)
       File.expects(:exist?).with('T001.spec').returns(false)
       # load the valid configuration
       assert_nothing_raised { cfg = Rutema::Configuration.new('full.rutema') }
-      assert_not_nil(cfg.parser)
-      assert_not_nil(cfg.reporters)
-      assert_equal(1, cfg.reporters.size)
+      assert_instance_of(Hash, cfg.context)
+      assert_equal({ key_a: 'Oops', key_b: 'Another value',
+                     key_c: 'One more value' }, cfg.context)
+      assert_instance_of(Hash, cfg.parser)
+      assert_equal(1, cfg.parser.size)
+      assert_equal({ class: Rutema::Parsers::SpecificationParser }, cfg.parser)
+      assert_instance_of(Hash, cfg.reporters)
+      assert_equal(2, cfg.reporters.size)
+      assert_equal({ Rutema::Reporters::BlockReporter \
+                     => { class: Rutema::Reporters::BlockReporter }, \
+                     Rutema::Reporters::EventReporter \
+                     => { class: Rutema::Reporters::EventReporter } }, cfg.reporters)
+      assert_instance_of(Hash, cfg.runner)
+      assert_equal(1, cfg.runner.size)
+      assert_equal({ class: Rutema::Runners::Default }, cfg.runner)
       assert_not_nil(cfg.tests)
-      assert_not_nil(cfg.context)
       assert_instance_of(OpenStruct, cfg.paths)
       assert_equal('/usr/share/doc', cfg.paths.doc)
       assert_equal('/usr/src', cfg.paths.src)
+      assert_equal(File.expand_path('setup.spec'), cfg.setup)
+      assert_equal(File.expand_path('suite_setup.spec'), cfg.suite_setup)
+      assert_equal(File.expand_path('suite_teardown.spec'), cfg.suite_teardown)
+      assert_equal(File.expand_path('teardown.spec'), cfg.teardown)
       assert_instance_of(OpenStruct, cfg.tools)
       assert_equal({ name: 'cat', path: '/usr/bin/cat', configuration: {} },
                    cfg.tools.cat)
