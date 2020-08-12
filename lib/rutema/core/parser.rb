9708765b250408df76a1069da9c9107f8e459215
markuspg <markuspg@users.noreply.github.com>
2020-08-12 23:00:18 +0200

    Fix RuboCop offenses for parser.rb

--- lib/rutema/core/parser.rb
+++ lib/rutema/core/parser.rb
@@ -1,35 +1,52 @@
 # Copyright (c) 2007-2020 Vassilis Rizopoulos. All rights reserved.
+
+# frozen_string_literal: true
+
 require_relative 'framework'
 
 module Rutema
-  module Parsers  
-    #Base class that bombs out when used.
+  module Parsers
+    ##
+    # Base class for parsers that raises exceptions if used directly
     #
-    #Derive your parser class from this class and implement parse_specification and validate_configuration
+    # Parser classes should be derived from this class and implement
+    # parse_specification and validate_configuration
     class SpecificationParser
       attr_reader :configuration
-      def initialize configuration
-        @configuration=configuration
-        @configuration||={}
+
+      def initialize(configuration)
+        @configuration = configuration
+        @configuration ||= {}
         validate_configuration
       end
-      #parses a specification
-      def parse_specification param
-        raise ParserError,"not implemented. You should derive a parser implementation from SpecificationParser!"
+
+      ##
+      # Parse a specification
+      def parse_specification(_param)
+        raise ParserError, \
+              'not implemented. You should derive a parser implementation from SpecificationParser!'
       end
-      #parses the setup script. By default calls parse_specification
-      def parse_setup param
+
+      ##
+      # Parse the setup script. By default calls parse_specification
+      def parse_setup(param)
         parse_specification(param)
       end
-      #parses the teardown script. By default calls parse_specification
-      def parse_teardown param
+
+      ##
+      # Parse the teardown script. By default calls parse_specification
+      def parse_teardown(param)
         parse_specification(param)
       end
-      #The parser stores it's configuration in @configuration
+
+      ##
+      # A parser stores its configuration in @configuration
       #
-      #To avoid validating the configuration in element_* methods repeatedly, do all configuration validation here
-      def validate_configuration
-      end
+      # To avoid validating the configuration in +element_*+ methods repeatedly,
+      # do all configuration validation here.
+      #
+      # The default implementation of this method does nothing.
+      def validate_configuration() end
     end
   end
 end
