b3948ce4ed4271fb04394185fe99062bfd6d99f0
markuspg <markuspg@users.noreply.github.com>
2020-08-12 22:46:32 +0200

    Fix RubyCop offenses for version.rb

--- lib/rutema/version.rb
+++ lib/rutema/version.rb
@@ -1,10 +1,14 @@
 # Copyright (c) 2007-2020 Vassilis Rizopoulos. All rights reserved.
+
+# frozen_string_literal: true
+
 module Rutema
-  #This module defines the version numbers for the library
+  ##
+  # This module defines the version numbers of the library
   module Version
-    MAJOR=2
-    MINOR=0
-    TINY=0
-    STRING=[ MAJOR, MINOR, TINY ].join( "." )
+    MAJOR = 2
+    MINOR = 0
+    TINY = 0
+    STRING = [MAJOR, MINOR, TINY].join('.')
   end
 end
