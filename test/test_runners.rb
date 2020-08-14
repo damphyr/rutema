394fc51399402d74a4ee56d97ae2126db5b7b1fd
markuspg <markuspg@users.noreply.github.com>
2020-08-14 22:43:03 +0200

    Fix RuboCop offenses for test_runners.rb

--- test/test_runners.rb
+++ test/test_runners.rb
@@ -1,4 +1,7 @@
 # Copyright (c) 2007-2020 Vassilis Rizopoulos. All rights reserved.
+
+# frozen_string_literal: true
+
 require 'ostruct'
 require 'test/unit'
 require 'mocha/test_unit'
@@ -8,29 +11,29 @@ require_relative '../lib/rutema/core/objectmodel'
 require_relative '../lib/rutema/core/runner'
 
 module TestRutema
-  class TestRunner<Test::Unit::TestCase
+  class TestRunner < Test::Unit::TestCase
     def test_new
-      scenario=Rutema::Scenario.new([Rutema::Step.new("desc")])
-      spec=Rutema::Specification.new(:scenario=>scenario)
-      queue=Queue.new
-      runner=Rutema::Runners::Default.new({},queue)
-      state=nil
-      assert_nothing_raised() { state=runner.run(spec) }
-      assert_equal(1, state["steps"].size)
+      scenario = Rutema::Scenario.new([Rutema::Step.new('desc')])
+      spec = Rutema::Specification.new(scenario: scenario)
+      queue = Queue.new
+      runner = Rutema::Runners::Default.new({}, queue)
+      state = nil
+      assert_nothing_raised { state = runner.run(spec) }
+      assert_equal(1, state['steps'].size)
       assert_equal(4, queue.size)
-      assert_equal("started", queue.pop.text)
-      2.times{queue.pop}
-      assert_equal("finished", queue.pop.text)
+      assert_equal('started', queue.pop.text)
+      2.times { queue.pop }
+      assert_equal('finished', queue.pop.text)
     end
-    
+
     def test_run_exceptions
-      step=Rutema::Step.new("bad",Patir::RubyCommand.new("bad"){|cmd| raise "Bad command"})
-      scenario=Rutema::Scenario.new([step])
-      spec=Rutema::Specification.new(:scenario=>scenario)
-      queue=Queue.new
-      runner=Rutema::Runners::Default.new({},queue)
-      state=nil
-      assert_nothing_raised() { state=runner.run(spec) }
+      step = Rutema::Step.new('bad', Patir::RubyCommand.new('bad') { raise 'Bad command' })
+      scenario = Rutema::Scenario.new([step])
+      spec = Rutema::Specification.new(scenario: scenario)
+      queue = Queue.new
+      runner = Rutema::Runners::Default.new({}, queue)
+      state = nil
+      assert_nothing_raised { state = runner.run(spec) }
       assert_equal(:error, state['status'])
     end
   end
