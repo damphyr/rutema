2b20f2600e3bb9de42e08d62801cb5d00a7f799b
markuspg <markuspg@users.noreply.github.com>
2020-08-15 17:18:29 +0200

    Add test for Rutema::RunnerMessage

--- test/test_messages.rb
+++ test/test_messages.rb
@@ -109,4 +109,70 @@ module TestRutema
       assert_equal('Msg Test Some sample text', msg.to_s)
     end
   end
+
+  class TestRunnerMessage < Test::Unit::TestCase
+    def test_initialize_default
+      # Test initialization
+      timestamp_before = Time.now
+      msg = assert_nothing_raised do
+        Rutema::RunnerMessage.new({})
+      end
+      timestamp_after = Time.now
+
+      # Verify created instance
+      assert_equal(0, msg.duration)
+      assert_equal('', msg.err)
+      assert_equal(1, msg.number)
+      assert_equal('', msg.out)
+      assert_equal(:none, msg.status)
+      assert_equal('', msg.test)
+      assert_equal('', msg.text)
+      assert((timestamp_before < msg.timestamp) \
+             && (timestamp_after > msg.timestamp))
+    end
+
+    def test_initialize_full
+      # Test initialization
+      timestamp = Time.now
+      msg = assert_nothing_raised do
+        Rutema::RunnerMessage.new({ 'duration' => 137, 'err' => 'Oops',
+                                    'number' => 45,
+                                    'out' => 'Something happened!',
+                                    'status' => :none,
+                                    test: 'Msg Init Example Test',
+                                    text: 'Some hypothetical message text',
+                                    timestamp: timestamp })
+      end
+
+      # Verify created instance
+      assert_equal(137, msg.duration)
+      assert_equal('Oops', msg.err)
+      assert_equal(45, msg.number)
+      assert_equal('Something happened!', msg.out)
+      assert_equal(:none, msg.status)
+      assert_equal('Msg Init Example Test', msg.test)
+      assert_equal('Some hypothetical message text', msg.text)
+      assert_equal(timestamp, msg.timestamp)
+    end
+
+    def test_stringification
+      # Create and verify minimal message
+      msg = Rutema::RunnerMessage.new({})
+      assert_equal('', msg.output)
+      assert_equal(':', msg.to_s)
+
+      # Create and verify a full message with all attributes set
+      timestamp = Time.now
+      msg = Rutema::RunnerMessage.new({ 'duration' => 137, 'err' => 'Oops',
+                                        'number' => 45,
+                                        'out' => 'Something happened!',
+                                        'status' => :error,
+                                        test: 'Msg Init Example Test',
+                                        text: 'Some hypothetical message text',
+                                        timestamp: timestamp })
+      assert_equal("Something happened!\nOops", msg.output)
+      assert_equal('Msg Init Example Test:Some hypothetical message text.' \
+                   + " Output:\nSomething happened!\nOops", msg.to_s)
+    end
+  end
 end
