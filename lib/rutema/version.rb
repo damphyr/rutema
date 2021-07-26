7fd818bf835d80b929de85cc0d984199b6c1ed9c
markuspg <markuspg@users.noreply.github.com>
2021-07-26 19:22:27 +0200

    Fix RuboCop offenses for version.rb

--- lib/rutema/version.rb
+++ lib/rutema/version.rb
@@ -6,15 +6,15 @@ module Rutema
   module Version
     ##
     # The major version of the rutema gem
-    MAJOR=2
+    MAJOR = 2
     ##
     # The minor version of the rutema gem
-    MINOR=0
+    MINOR = 0
     ##
     # The tiny version of the rutema gem
-    TINY=0
+    TINY = 0
     ##
     # The version information of the rutema gem as a string
-    STRING=[ MAJOR, MINOR, TINY ].join( "." )
+    STRING = [MAJOR, MINOR, TINY].join(".")
   end
 end
